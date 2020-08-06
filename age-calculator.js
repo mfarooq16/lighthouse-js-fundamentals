f146a1d5eb7f2304f8dd495353620fa7ae917ff6
vagrant <maham.16491@gmail.com>
2020-08-06 22:29:09 +0000

    Calculate the age through subtracting years

---
+++ age-calculator.js
@@ -0,0 +1,12 @@
+let name = ""
+let yearOfBirth = 0
+let currentYear = 0
+
+function ageCalculator (name, yearOfBirth, currentYear) {
+  let age = currentYear - yearOfBirth
+  
+  return (name + " is " + age + " years old.")
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));

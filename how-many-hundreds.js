0ba7e9b6a166e3be3417f9b85e7577ec1140d2fa
vagrant <maham.16491@gmail.com>
2020-08-06 22:53:19 +0000

    how-many-hundreds in a number

---
+++ how-many-hundreds.js
@@ -0,0 +1,14 @@
+let num = 0;
+
+function howManyHundreds (num) {
+  let newNum = ((num - (num % 100)) / 100);
+
+  return newNum;
+}
+
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
